d9784f715514c28c705edd3c2a90e71adfa9423b
Fin12n <thinhminecraftpe1@gmail.com>
2024-10-16 20:56:14 +0700

    Create Emergency Emden.lua

---
+++ Emergency Emden.lua
@@ -0,0 +1,8 @@
+-- Script Aimbot:
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Sheeshablee73/Scriptss/main/Gun Grounds FFA.lua"))()
+-- Script ESP:
+pcall(function() loadstring(game:HttpGet('https://raw.githubusercontent.com/ic3w0lf22/Unnamed-ESP/master/UnnamedESP.lua'))() end)
+-- Script Speed & TP
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Bac0nHck/Scripts/ade1f8efcb4604be34c29476a5baebae4c0ee9d6/ScaryHideSeek"))("t.me/arceusxscripts")
+-- Script FreeCam
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Fin12n/Script/refs/heads/main/FreeCam.lua"))()
