27625c229e12900c3e61c7f6fa3f903874243c3f
Fin12n <thinhminecraftpe1@gmail.com>
2024-10-20 15:05:25 +0700

    Update Emergency Emden.lua

--- Emergency Emden.lua
+++ Emergency Emden.lua
@@ -6,3 +6,5 @@ pcall(function() loadstring(game:HttpGet('https://raw.githubusercontent.com/ic3w
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Bac0nHck/Scripts/ade1f8efcb4604be34c29476a5baebae4c0ee9d6/ScaryHideSeek"))("t.me/arceusxscripts")
 -- Script FreeCam
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Fin12n/Script/refs/heads/main/FreeCam.lua"))()
+-- Script Inf Jump
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Fin12n/Script/refs/heads/main/Inf%20Jump.lua"))()
